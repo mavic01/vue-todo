5deb692b2db0585809321034bf6a256dd5809f22
Victor Iberi <iberivictor7@gmail.com>
2025-09-26 18:40:47 +0100

    made changes

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
-import './style.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+console.log("Supabase URL:", import.meta.env.VITE_SUPABASE_URL)
+console.log("Supabase Key:", import.meta.env.VITE_SUPABASE_ANON_KEY)
+
 createApp(App).use(router).mount('#app')
+
 
